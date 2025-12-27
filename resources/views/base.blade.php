9a68325fd34d7341f501ba881891ed0265497e0d
Nostres25 <no04so255@gmail.com>
2025-12-28 00:37:53 +0100

    build: :heavy_plus_sign: Ajout de bootstrap via composer
    Jquery a aussi été ajouté car bootstrap en a besoin. Bootstrap a été installé manuellement via composer car nous avons besoin d'un framework css pour développer rapidement et efficacement sans perdre du temps avec du css. Or, nous ne pouvon utiliser Node.js en production. Puisque je n'ai pas trouvé comment installer tailwindcss sans Node.js, j'ai choisi d'installer bootstrap avec composer

--- resources/views/base.blade.php
+++ resources/views/base.blade.php
@@ -4,13 +4,41 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Suivi colis IUTV</title>
-    @yield('css')
+    {{-- TODO retirer ça --}}
+     {{-- @yield('css') --}}
+     {{-- Autre solution, utiliser notre propre CSS --}}
+    {{-- <link rel="stylesheet" href="{{ asset('css/app.css') }}"> --}}
+    {{-- Autre solution, installer tailwind sans nodejs : https://tailwindcss.com/blog/standalone-cli --}}
+
+    {{-- Autre solution : utiliser le CDN de tailwind mais pas recommandé pour la production :/ --}}
+
+    {{-- <script src="https://cdn.jsdelivr.net/npm/@tailwindcss/browser@4"></script>
+    <style type="text/tailwindcss">
+      @theme {
+        --color-clifford: #da373d;
+      }    
+    </style> --}}
+
+    {{-- Bootstrap installé localement avec jquery et popper (intégré à bootstrap.bundle.min.js) nécéssaires pour bootstrap --}}
+
+    {{-- Pour charger la feuille de style bootstrap --}}
+    <link href="bootstrap/css/bootstrap.min.css" rel="stylesheet">
+    {{-- <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script> --}}
+    
+
+    {{-- Pour charger les scripts jquery, nécéssaire à boostrap --}}
+    <script type="text/javascript" src="jquery/jquery.min.js"></script>
+
+    {{-- Pour charger popper, nécéssaire à boostrap --}}
+    <script type="text/javascript" src="bootstrap/js/bootstrap.bundle.min.js"></script>
+
+    {{--  Pour charger les scripts boostrap--}}
+    <script type="text/javascript" src="bootstrap/js/bootstrap.min.js"></script>
+    
 </head>
 <body>
 <header>
-  <div>
-    <h1>Logiciel de suivi des colis à l'IUT de Villetaneuse</h1>
-  </div>
+  <x-nav></x-nav>
 </header>
 <main>
     @yield('content')
