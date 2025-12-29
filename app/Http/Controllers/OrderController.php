c4bdcbbedbd2ed2afb7ed2222b4d89b82b93a738
Nostres25 <no04so255@gmail.com>
2025-12-29 15:47:01 +0100

    feat: :sparkles: Ajout modal création commande

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -82,7 +82,41 @@ class OrderController extends Controller
             ],
         ];
 
-        return view('orders', ['orders' => $orders]);
+        if (session()->get('user')->getRole() === 'Administrateur BD') {
+            //trie d'une certaine manière
+        }
+
+        return view('orders', [
+            'orders' => $orders,
+            'orderStates' => [
+                'BROUILLON', // enregistré à l'état de brouillon. Affiché seulement pour le demandeur
+
+                'DEVIS', // à l'état de devis ; en attente d'un bon de commande (première étape)
+
+                'DEVIS_REFUSE', // à l'état de devis ; l'éditeur de bon de commande a refusé de faire un bon de commande
+
+                'BON_DE_COMMANDE_NON_SIGNE', // à l'état de bon de commande ; doit être signé par le directeur
+
+                'BON_DE_COMMANDE_REFUSE', // à l'état de bon de commande ; le directeur a refusé de signer
+
+                'BON_DE_COMMANDE_SIGNE', // à l'état de bon de commande signé ; en attente d'envoi du bon signé de commande au fournisseur
+
+                'COMMANDE', // à l'état de bon de commande signé ; commmandé sans réponse, en attente de réponse du fournisseur
+
+                'COMMANDE_REFUSEE', // à l'état de bon de commande signé ; commande refusée par le fournisseur
+
+                'COMMANDE_AVEC_REPONSE', // à l'état de bon de commande signé ; le fournisseur a répondu favorablement à la commande. (Peut fournir le délai de livraison)
+
+                'PARTIELLEMENT_LIVRE', // le demandeur à signalé que certains colis ont été livrés, et que d'autres sont manquants.
+
+                'SERVICE_FAIT', // = terme utilisé par le demandeur pour signaler que la commande a été totalement livrée ; en attente de paiment par le service financier
+
+                'LIVRE_ET_PAYE', // commande payée par le service financié (dernière étape)
+
+                'ANNULE', // La commande a été annulée par le demandeur à n'importe quelle étape
+            ],
+            'defaultOrderState' => 'BROUILLON',
+        ]);
     }
 
     public function newOrder(): View
