e932a1dbfe13f66020164df73a5e9f2c1f50eb84
Nostres25 <no04so255@gmail.com>
2025-12-29 15:46:33 +0100

    build: :white_check_mark: Modifications bootstrap

--- resources/views/base.blade.php
+++ resources/views/base.blade.php
@@ -4,37 +4,9 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Suivi colis IUTV</title>
-    {{-- TODO retirer ça --}}
-     {{-- @yield('css') --}}
-     {{-- Autre solution, utiliser notre propre CSS --}}
-    {{-- <link rel="stylesheet" href="{{ asset('css/app.css') }}"> --}}
-    {{-- Autre solution, installer tailwind sans nodejs : https://tailwindcss.com/blog/standalone-cli --}}
-
-    {{-- Autre solution : utiliser le CDN de tailwind mais pas recommandé pour la production :/ --}}
-
-    {{-- <script src="https://cdn.jsdelivr.net/npm/@tailwindcss/browser@4"></script>
-    <style type="text/tailwindcss">
-      @theme {
-        --color-clifford: #da373d;
-      }    
-    </style> --}}
-
-    {{-- Bootstrap installé localement avec jquery et popper (intégré à bootstrap.bundle.min.js) nécéssaires pour bootstrap --}}
-
-    {{-- Pour charger la feuille de style bootstrap --}}
     <link href="bootstrap/css/bootstrap.min.css" rel="stylesheet">
-    {{-- <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script> --}}
-    
-
-    {{-- Pour charger les scripts jquery, nécéssaire à boostrap - c'était nécéssaire avec bootstrap 4 mais plus avec boostrap 5--}}
-    {{-- <script type="text/javascript" src="jquery/jquery.min.js"></script> --}}
-
-    {{-- Pour charger jquery popper et bootstrap en même temps, tout est nécéssaire à boostrap --}}
     <script type="text/javascript" src="bootstrap/js/bootstrap.bundle.min.js"></script>
-
-    {{--  Pour charger les scripts boostrap - c'était nécéssaire avec bootstrap 4 mais plus avec boostrap 5--}}
-    {{-- <script type="text/javascript" src="bootstrap/js/bootstrap.min.js"></script> --}}
-    
+    {{-- <script src="../ressources/js/app.js"></script> --}}
 </head>
 <body>
 <header>
@@ -45,4 +17,14 @@
 </main>
 <footer></footer>
 </body>
+<style>
+  /* Correction d'un bug visuel Bootstrap 5.3.8 qui décale la page en fond lors de l'ouverture d'un modal sur petits écrans (mobile) */
+  .modal-open {
+    padding-right: 0 !important;
+  }
+
+  html {
+    overflow-y: scroll !important;
+  }
+</style>
 </html>
