9d7ed94184200ce0b5bfae24c219d96c2c71f394
WeameELM <Weameelmouttaqui@gmail.com>
2025-12-30 17:26:47 +0100

    About page with a modern designed

--- routes/web.php
+++ routes/web.php
@@ -5,13 +5,10 @@ use Illuminate\Support\Facades\Route;
 Route::get('login', [\App\Http\Controllers\AuthController::class, 'auth']);
 
 Route::get('/', [\App\Http\Controllers\OrderController::class, 'viewOrders']);
-Route::post('/', [\App\Http\Controllers\OrderController::class, 'viewOrders']);
 
 // TODO maybe prefer to use "create" instead of "register"
 Route::get('orders/new', [\App\Http\Controllers\OrderController::class, 'newOrder']);
 Route::post('orders/new', [\App\Http\Controllers\OrderController::class, 'submitNewOrder']);
-
-Route::get('suppliers', [\App\Http\Controllers\OrderController::class, 'viewSuppliers']);
-
+Route::get('/about', [\App\Http\Controllers\AboutController::class, 'about']);
 // Only for tests:
 // Route::get('/', [\App\Http\Controllers\HomeController::class, 'home']);
