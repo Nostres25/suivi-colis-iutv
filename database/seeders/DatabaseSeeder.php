8ce80fb8fa8b90aa77dff3a0791300231ade9410
Nostres25 <no04so255@gmail.com>
2025-12-31 01:15:12 +0100

    feat: :poop: Seeders avancement
    Non fonctionnel pour le moment. Le remplissage des rôles et permissions par défaut est opérationnel mais pour les données de test il reste des mystères à résoudre bien que l'avancement soit satisfaisant

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -5,154 +5,214 @@ namespace Database\Seeders;
 use App\Models\Role;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\App;
+use Illuminate\Support\Facades\DB;
 
+// Permissions
+// - Consulter toutes les commandes      => Pouvoir voir et rechercher toutes les commandes dans le système.
+// - Consulter ses commandes             => Pouvoir voir uniquement les commandes appartenant au même département/service.
+// - Créer des commandes                 => Pouvoir créer des commandes à l'état de devis.
+// - Modifier des commandes              => Pouvoir modifier les informations principales d'une commande.
+// - Ajouter un bon de livraison         => Pouvoir marquer les colis respectifs comme livrés et ajouter un bon de livraison.
+// - Notes et commentaires               => Pouvoir ajouter des commentaires et modifier la note pour les commandes et les fournisseurs.
+// - Demander l'ajout d'un fournisseur   => Pouvoir demander l'ajout d'un fournisseur au service financier.
+// - Consulter la liste des fournisseurs => Pouvoir consulter la liste des fournisseurs valides.
+// - Gérer les fournisseurs              => Pouvoir ajouter, modifier et valider ou invalider les fournisseurs.
+// - Gérer les bons de commande          => Pouvoir ajouter, refuser, modifier et supprimer des bons de commande.
+// - Payer les fournisseurs              => Pouvoir marquer les commandes comme payées.
+// - Admin                               => Avoir tous les accès et pouvoir gérer la base de données.
+enum Permission: int
+{
+    case CONSULTER_COMMANDES = 2;
+    case CONSULTER_SES_COMMANDES = 3;
+    case CREER_COMMANDES = 4;
+    case MODIFIER_COMMANDES = 5;
+    case AJOUTER_BON_DE_LIVRAISON = 6;
+
+    case NOTES_ET_COMMENTAIRES = 7;
+    case DEMANDER_AJOUT_FOURNISSEUR = 8;
+    case CONSULTER_LISTE_FOURNISSEURS = 9;
+    case GERER_FOURNISSEUR = 10;
+    case GERER_BONS_DE_COMMANDES = 11;
+    case PAYER_FOURNISSEUR = 12;
+
+    case ADMIN = 1;
+}
 class DatabaseSeeder extends Seeder
 {
     public function run(): void
     {
         // Données par défaut générales
 
-        // ANCIEN CODE
-        // // Rôles par défaut
-        // $roles = [
-        //     ['name' => 'Administrateur BD', 'description' => 'Accès total à la base de données.'],
-        //     ['name' => 'Responsable colis', 'description' => 'S\'occupe de livrer les colis aux départements respectifs.'],
-        //     ['name' => 'Service financier', 'description' => 'S\'occupe de la liste des fournisseurs valides, des bons de commandes et de payer le fournisseur.'],
-        //     ['name' => 'Département Info', 'description' => 'Membre du département informatique.'],
-        //     ['name' => 'Département GEA', 'description' => 'Membre du département GEA.'],
-        //     ['name' => 'Département CJ', 'description' => 'Membre du département CJ.'],
-        //     ['name' => 'Département GEII', 'description' => 'Membre du département GEII.'],
-        //     ['name' => 'Département RT', 'description' => 'Membre du département réseaux et télécommunications.'],
-        //     ['name' => 'Département SD', 'description' => 'Membre du département sciences des données.'],
-        // ];
-        // Role::upsert($roles, uniqueBy: ['name'], update: ['description']);
-
-        // // Permissions par défaut:
-        // // - Consulter toutes les commandes      => Pouvoir voir et rechercher toutes les commandes dans le système.
-        // // - Consulter ses commandes             => Pouvoir voir uniquement les commandes appartenant au même département/service.
-        // // - Créer des commandes                 => Pouvoir créer des commandes à l'état de devis.
-        // // - Modifier des commandes              => Pouvoir modifier les informations principales d'une commande.
-        // // - Ajouter un bon de livraison         => Pouvoir marquer les colis respectifs comme livrés et ajouter un bon de livraison.
-        // // - Notes et commentaires               => Pouvoir ajouter des commentaires et modifier la note pour les commandes et les fournisseurs.
-        // // - Demander l'ajout d'un fournisseur   => Pouvoir demander l'ajout d'un fournisseur au service financier.
-        // // - Consulter la liste des fournisseurs => Pouvoir consulter la liste des fournisseurs valides.
-        // // - Gérer les fournisseurs              => Pouvoir ajouter, modifier et valider ou invalider les fournisseurs.
-        // // - Gérer les bons de commande          => Pouvoir ajouter, refuser, modifier et supprimer des bons de commande.
-        // // - Payer les fournisseurs              => Pouvoir marquer les commandes comme payées.
-        // // - Admin                               => Avoir tous les accès et pouvoir gérer la base de données.
-        // $permissions = [
-        //     ['label' => 'Consulter toutes les commandes'],
-        //     ['label' => 'Consulter ses commandes'],
-        //     ['label' => 'Créer des commandes'],
-        //     ['label' => 'Modifier des commandes'],
-        //     ['label' => 'Ajouter un bon de livraison'],
-
-        //     ['label' => 'Notes et commentaires'],
-
-        //     ['label' => 'Demander l\'ajout d\'un fournisseur'],
-        //     ['label' => 'Consulter la liste des fournisseurs'],
-
-        //     ['label' => 'Gérer les fournisseurs'],
-        //     ['label' => 'Gérer les bons de commande'],
-        //     ['label' => 'Payer les fournisseurs'],
-
-        //     ['label' => 'Admin'],
-        // ];
-
-        // DB::table('permissions')->upsert($permissions);
-
-        // // Attribution de permissions par défaut
-        // DB::table('permission_role')->insert([
-        //     ['role_id' => 1, 'permission_id' => 1],
-
-        // ]);
-
-        // Nouvelles données - TODO soit garder ce principe bizarre soit revenir comme avant carr là c'est galère
         // Rôles par défaut-
-        $roles = [
-            'Administrateur BD' => [
+        $roles = collect([
+            [
+                'name' => 'Administrateur BD',
+                'id' => 1,
                 'description' => 'Accès total à la base de données.',
-                'permissions' => [],
+                'permissions' => [
+                    Permission::CONSULTER_COMMANDES,
+                ],
             ],
-            'Responsable colis' => [
+            [
+                'name' => 'Responsable colis',
+                'id' => 2,
                 'description' => 'S\'occupe de livrer les colis aux départements respectifs.',
-                'permissions' => [],
+                'permissions' => [
+                    Permission::CONSULTER_COMMANDES,
+                ],
             ],
-            'Service financier' => [
+            [
+                'name' => 'Service financier',
+                'id' => 3,
                 'description' => 'S\'occupe de la liste des fournisseurs valides, des bons de commandes et de payer le fournisseur.',
-                'permissions' => [],
+                'permissions' => [
+                    Permission::CONSULTER_COMMANDES,
+                ],
             ],
-            'Département Info' => [
+            [
+                'name' => 'Département Info',
+                'id' => 4,
                 'description' => 'Membre du département informatique.',
-                'permissions' => [],
+                'permissions' => [
+                    Permission::CONSULTER_COMMANDES,
+                ],
             ],
-            'Département GEA' => [
+            [
+                'name' => 'Département GEA',
+                'id' => 5,
                 'description' => 'Membre du département GEA.',
-                'permissions' => [],
+                'permissions' => [
+                    Permission::CONSULTER_COMMANDES,
+                ],
             ],
-            'Département CJ' => [
+            [
+                'name' => 'Département CJ',
+                'id' => 6,
                 'description' => 'Membre du département CJ.',
-                'permissions' => [],
+                'permissions' => [
+                    Permission::CONSULTER_COMMANDES,
+                ],
             ],
-            'Département GEII' => [
+            [
+                'name' => 'Département GEII',
+                'id' => 7,
                 'description' => 'Membre du département GEII.',
-                'permissions' => [],
+                'permissions' => [
+                    Permission::CONSULTER_COMMANDES,
+                ],
             ],
-            'Département RT' => [
+            [
+                'name' => 'Département RT',
+                'id' => 8,
                 'description' => 'Membre du département réseaux et télécommunications.',
-                'permissions' => [],
+                'permissions' => [
+                    Permission::CONSULTER_COMMANDES,
+                ],
             ],
-            'Département SD' => [
+            [
+                'name' => 'Département SD',
+                'id' => 9,
                 'description' => 'Membre du département sciences des données.',
-                'permissions' => [],
+                'permissions' => [
+                    Permission::CONSULTER_COMMANDES,
+                ],
             ],
-        ];
+        ]);
 
-        $roleElements = [];
-        foreach ($roles as $role) {
-            array_push($roleElements, ['label' => key($role), 'description' => $role['description']]);
+        $roles = $roles->sort(function ($role1, $role2) {
+            return $role1['id'] - $role2['id'];
+        });
+
+        //        $roles = [
+        //            'Administrateur BD' => [
+        //                'description' => 'Accès total à la base de données.',
+        //                'permissions' => [],
+        //            ],
+        //            'Responsable colis' => [
+        //                'description' => 'S\'occupe de livrer les colis aux départements respectifs.',
+        //                'permissions' => [],
+        //            ],
+        //            'Service financier' => [
+        //                'description' => 'S\'occupe de la liste des fournisseurs valides, des bons de commandes et de payer le fournisseur.',
+        //                'permissions' => [],
+        //            ],
+        //            'Département Info' => [
+        //                'description' => 'Membre du département informatique.',
+        //                'permissions' => [],
+        //            ],
+        //            'Département GEA' => [
+        //                'description' => 'Membre du département GEA.',
+        //                'permissions' => [],
+        //            ],
+        //            'Département CJ' => [
+        //                'description' => 'Membre du département CJ.',
+        //                'permissions' => [],
+        //            ],
+        //            'Département GEII' => [
+        //                'description' => 'Membre du département GEII.',
+        //                'permissions' => [],
+        //            ],
+        //            'Département RT' => [
+        //                'description' => 'Membre du département réseaux et télécommunications.',
+        //                'permissions' => [],
+        //            ],
+        //            'Département SD' => [
+        //                'description' => 'Membre du département sciences des données.',
+        //                'permissions' => [],
+        //            ],
+        //        ];
+
+        //        $roleElements = [];
+        //        foreach ($roles as $role) {
+        //            $roleElements[] = ['label' => key($role), 'description' => $role['description']];
+        //        }
+        Role::upsert($roles->map(function ($role) {
+            return ['name' => $role['name'], 'description' => $role['description']];
+        })->toArray(), uniqueBy: ['name'], update: ['description']);
+
+        //        $permissions = [
+        //            ['label' => 'Consulter toutes les commandes'],
+        //            ['label' => 'Consulter ses commandes'],
+        //            ['label' => 'Créer des commandes'],
+        //            ['label' => 'Modifier des commandes'],
+        //            ['label' => 'Ajouter un bon de livraison'],
+        //
+        //            ['label' => 'Notes et commentaires'],
+        //
+        //            ['label' => 'Demander l\'ajout d\'un fournisseur'],
+        //            ['label' => 'Consulter la liste des fournisseurs'],
+        //
+        //            ['label' => 'Gérer les fournisseurs'],
+        //            ['label' => 'Gérer les bons de commande'],
+        //            ['label' => 'Payer les fournisseurs'],
+        //
+        //            ['label' => 'Admin'],
+        //        ];
+
+        $permissions = Permission::cases();
+        sort($permissions);
+        $permissionElements = [];
+        foreach ($permissions as $permission) {
+            $permissionElements[] = ['label' => $permission];
         }
-        Role::upsert($roleElements, uniqueBy: ['name'], update: ['description']);
-
-        // Permissions par défaut:
-        // - Consulter toutes les commandes      => Pouvoir voir et rechercher toutes les commandes dans le système.
-        // - Consulter ses commandes             => Pouvoir voir uniquement les commandes appartenant au même département/service.
-        // - Créer des commandes                 => Pouvoir créer des commandes à l'état de devis.
-        // - Modifier des commandes              => Pouvoir modifier les informations principales d'une commande.
-        // - Ajouter un bon de livraison         => Pouvoir marquer les colis respectifs comme livrés et ajouter un bon de livraison.
-        // - Notes et commentaires               => Pouvoir ajouter des commentaires et modifier la note pour les commandes et les fournisseurs.
-        // - Demander l'ajout d'un fournisseur   => Pouvoir demander l'ajout d'un fournisseur au service financier.
-        // - Consulter la liste des fournisseurs => Pouvoir consulter la liste des fournisseurs valides.
-        // - Gérer les fournisseurs              => Pouvoir ajouter, modifier et valider ou invalider les fournisseurs.
-        // - Gérer les bons de commande          => Pouvoir ajouter, refuser, modifier et supprimer des bons de commande.
-        // - Payer les fournisseurs              => Pouvoir marquer les commandes comme payées.
-        // - Admin                               => Avoir tous les accès et pouvoir gérer la base de données.
-        $permissions = [
-            ['label' => 'Consulter toutes les commandes'],
-            ['label' => 'Consulter ses commandes'],
-            ['label' => 'Créer des commandes'],
-            ['label' => 'Modifier des commandes'],
-            ['label' => 'Ajouter un bon de livraison'],
-
-            ['label' => 'Notes et commentaires'],
-
-            ['label' => 'Demander l\'ajout d\'un fournisseur'],
-            ['label' => 'Consulter la liste des fournisseurs'],
-
-            ['label' => 'Gérer les fournisseurs'],
-            ['label' => 'Gérer les bons de commande'],
-            ['label' => 'Payer les fournisseurs'],
-
-            ['label' => 'Admin'],
-        ];
-
-        DB::table('permissions')->upsert($permissions);
+        DB::table('permissions')->upsert($permissionElements, uniqueBy: ['label']);
 
         // Attribution de permissions par défaut
-        DB::table('permission_role')->insert([
-            ['role_id' => $roles->filter, 'permission_id'],
+        $permission_roleElements = [];
+        $i = 1;
+        foreach ($roles as $role) {
+            foreach ($role['permissions'] as $permission) {
 
-        ]);
+                $permission_roleElements[] = ['permission_id' => $permission, 'role_id' => $i];
+            }
+            $i++;
+        }
+
+        if (! empty($permission_roleElements)) {
+            DB::table('permission_role')->upsert($permission_roleElements, uniqueBy: ['permission_id', 'role_id']);
+        }
+
+        // Addition seeders
 
         if (App::environment('local', 'staging', 'testing')) {
             $this->call([
--- database/seeders/LocalTestSeeder.php
+++ database/seeders/LocalTestSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Comment;
 use App\Models\Log;
 use App\Models\Order;
 use App\Models\Package;
@@ -20,43 +21,70 @@ class LocalTestSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
 
         $nbSuppliers = 10;
         $nbUsers = 20;
         $nbOrders = 50;
 
-        $suppliers = Supplier::factory($nbSuppliers)->create();
-        // ->hasAttached(fake()->boolean(80) ? 1 : $roles->random_int(2, 3))
+        $orders = Order::factory($nbOrders)->create();
+        //            ->make()
+        //            ->each(function (Order $order) use ($suppliers) {
+        //                $order->supplier()->($suppliers->random());
+        //                $order->save(); // utile de les sauvegarder un par un ?
+        //            });
 
-        $users = User::factory($nbUsers)->create()->each(function (User $user) {
-
-            // Chaque utilisateur doit avoir au moins un rôle aléatoire
-            $roles = [
-                Role::all()->random(),
-            ];
-
-            // et dans 10% des cas un utilisateur a UN AUTRE, aussi aléatoire
-            $role2 = Role::all()->random();
-            if (random_int(0, 100) <= 10 && $roles[0] !== $role2) {
-                array_push($roles, $role2);
-            }
-
-            $user->roles()->sync($roles);
-        });
-
-        $orders = Order::factory($nbOrders)
-            ->hasAttached($suppliers->random())
+        $suppliers = Supplier::factory($nbSuppliers)
+            ->hasOrders(7)
             ->create();
 
-        $packages = Package::factory($nbOrders * random_int(2, 5))
-            ->hasAttached($orders->random())
-            ->create();
+        $comments = Comment::factory($nbUsers * rand(2, 6))
+            ->make()
+            ->each(function (Comment $comment) use ($orders) {
+                $comment->order()->associate($orders->random());
+            });
 
-        $logs = Log::factory($nbUsers * random_int(2, 6))
-            ->hasAttached($users->random()) // users avec le rôle d'un département
-            ->hasAttached($orders->random())
-            ->create();
+        $users = User::factory($nbUsers)
+            ->create()
+            ->each(function (User $user) use ($comments) {
+                // Chaque utilisateur doit avoir au moins un rôle aléatoire
+                $roles = [
+                    Role::all()->random(),
+                ];
+
+                // et dans 10% des cas un utilisateur a UN AUTRE, aussi aléatoire
+                $role2 = Role::all()->random();
+                $hasSecondRole = rand(0, 100) <= 10 && $roles[0] !== $role2;
+                if ($hasSecondRole) {
+                    $roles[] = $role2;
+                }
+
+                $user->roles()->sync($roles);
+
+                // Chaque utilisateur peut avoir plusieurs commentaires uniquement sur les commandes créés par un membre du même rôle/département
+                $user->hasMany($comments->filter(
+                    function (Comment $comment) use ($roles, $hasSecondRole) {
+                        // TODO pas sûr d'avoir bien compris le principe de BelongsTo, HasMany et compagnie. À VOIR
+                        dd($comment->order());
+
+                        return ! $comment->order()->logs()->where('id', 1)->user()->roles()->find(function (Role $role) use ($roles, $hasSecondRole) {
+                            return $role === $roles[0] || ($hasSecondRole && $role === $roles[1]);
+                        });
+                    }
+                )->random());
+            });
+
+        $packages = Package::factory($nbOrders * rand(2, 5))
+            ->make()
+            ->each(function (Package $p) use ($orders) {
+                $p->order()->associate($orders->random());
+                $p->save();
+            });
+
+        $logs = Log::factory($nbUsers * rand(2, 6))
+            ->make()
+            ->each(function (Log $log) use ($orders) {
+                $log->order()->associate($orders->random());
+            });
 
     }
 }
