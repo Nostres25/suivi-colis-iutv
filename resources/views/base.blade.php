17eaa89cb835e19d6a93e6e9dce6a9a898bd1322
WeameELM <Weameelmouttaqui@gmail.com>
2026-01-15 11:14:17 +0100

    Logo ajouté et aesthetic changé

--- resources/views/base.blade.php
+++ resources/views/base.blade.php
@@ -7,6 +7,7 @@
     <link href="{{asset('bootstrap/css/bootstrap.min.css')}}" rel="stylesheet">
     <script type="text/javascript" src="{{asset('bootstrap/js/bootstrap.bundle.min.js')}}"></script>
     <link href="{{asset('style.css')}}" rel="stylesheet" type="text/css">
+    <link rel="icon" type="image/png" href="{{ asset('logo.png') }}">
 </head>
 <body>
 <header class="mb-5">
--- resources/views/components/alert.blade.php
+++ resources/views/components/alert.blade.php
@@ -1,3 +1,1 @@
-<div class="alert alert-success mb-0">
-    {{$alertMessage}}
-</div>
+
